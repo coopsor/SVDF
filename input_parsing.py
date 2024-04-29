d0adc126bcbdd02a8983e8dfc787444ae6c8ff50
hh22814 <67181609+hh22814@users.noreply.github.com>
2024-04-29 16:41:11 +0800

    Update input_parsing.py

--- input_parsing.py
+++ input_parsing.py
@@ -46,6 +46,7 @@ def parse_arguments(arguments = sys.argv[1:]):
                                   DUP:INT (interspersed duplications), BND (breakends).')
     parser_bam.add_argument('--ref',
                             type=str,
+                            nargs='*',
                             default=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', 'X',
                                      'Y', 'chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10',
                                      'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19',
